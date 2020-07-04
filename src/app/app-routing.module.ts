9a5989e8e3e2b47d544176ef018f894560bca6bc
ivan hernandez <ihernandezt@unal.edu.co>
2020-07-04 17:06:30 -0500

    listo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'mapa',
+    redirectTo: 'places',
     pathMatch: 'full'
   },
   {
