6580a7c5bf04ced2be494d2fc1c49700803385af
juanfer <jramirezmon@unal.edu.co>
2020-07-04 15:27:38 -0500

    Modificaciones a places

--- src/app/places/places.page.ts
+++ src/app/places/places.page.ts
@@ -9,14 +9,14 @@ private lugares=[
 {
    id: '1',
    title: 'Campus un',
-   imageURL: 'https://www.eltiempo.com/files/image_640_428/uploads/2017/09/21/59c45ca7ae8a2.jpeg',
+   imagenURL: 'https://www.eltiempo.com/files/image_640_428/uploads/2017/09/21/59c45ca7ae8a2.jpeg',
    comments: ['sfefef','sfefefe']
 
 },
 {
   id: '2',
   title: 'Edificio Insignio Julio Garavito',
-  imageURL: 'https://d2yoo3qu6vrk5d.cloudfront.net/images/20181204071023/senjal-colombia-20170921-00-482x320.jpg',
+  imagenURL: 'https://d2yoo3qu6vrk5d.cloudfront.net/images/20181204071023/senjal-colombia-20170921-00-482x320.jpg',
   comments: ['Edificio insigne de la facultad de ingeniería','sfdfsf']
 }
 ]
